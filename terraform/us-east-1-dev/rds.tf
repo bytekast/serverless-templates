440f3e0fde4eaa43447fd0ad7adfb9173383d3fb
Rowell Belen <developer@bytekast.com>
2018-02-17 20:35:25 -0700

    Create simple VPC and RDS

--- terraform/us-east-1-dev/rds.tf
+++ terraform/us-east-1-dev/rds.tf
@@ -1,6 +1,6 @@
 variable "demo_db_password" {}
 
-resource "aws_db_instance" "demo-db" {
+resource "aws_db_instance" "demo_db" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
@@ -12,4 +12,23 @@ resource "aws_db_instance" "demo-db" {
   password             = "${var.demo_db_password}"
   final_snapshot_identifier = "${var.env}-demo-db-instance-final"
   publicly_accessible = true
+  vpc_security_group_ids = ["${aws_security_group.default_private_sg.id}"]
+  db_subnet_group_name   = "${aws_db_subnet_group.rds_subnet_group.id}"
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds_subnet_group"
+  subnet_ids = ["${aws_subnet.private_subnet_us_east_1b.id}", "${aws_subnet.private_subnet_us_east_1c.id}"]
+
+  tags {
+    Name = "rds-subnet-group"
+  }
+}
+
+output "demo_db_instance_id" {
+  value = "${aws_db_instance.demo_db.id}"
+}
+
+output "demo_db_instance_address" {
+  value = "${aws_db_instance.demo_db.address}"
 }
---
+++ terraform/us-east-1-dev/vpc.tf
@@ -0,0 +1,154 @@
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for the public subnet"
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet1_cidr" {
+  description = "CIDR for the private subnet"
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet2_cidr" {
+  description = "CIDR for the private subnet"
+  default = "10.0.3.0/24"
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet_us_east_1a" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "${var.public_subnet_cidr}"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  tags = {
+    Name =  "Public Subnet (us-east-1a)"
+  }
+}
+
+resource "aws_subnet" "private_subnet_us_east_1b" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "${var.private_subnet1_cidr}"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name =  "Private Subnet (us-east-1b)"
+  }
+}
+
+resource "aws_subnet" "private_subnet_us_east_1c" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "${var.private_subnet2_cidr}"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name =  "Private Subnet (us-east-1c)"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "Default Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "Public Subnet Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public-rt" {
+  subnet_id = "${aws_subnet.public_subnet_us_east_1a.id}"
+  route_table_id = "${aws_route_table.public-rt.id}"
+}
+
+
+resource "aws_security_group" "default_public_sg" {
+  name = "default_public_sg"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  vpc_id="${aws_vpc.default.id}"
+
+  tags {
+    Name = "Default Public Security Group"
+  }
+}
+
+resource "aws_security_group" "default_private_sg" {
+  name = "default_private_sg"
+  description = "Allow traffic from public subnet"
+
+  ingress {
+    from_port = 5434
+    to_port = 5434
+    protocol = "tcp"
+    cidr_blocks = ["${var.public_subnet_cidr}"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["${var.public_subnet_cidr}"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.public_subnet_cidr}"]
+  }
+
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "Default Private Security Group"
+  }
+}
