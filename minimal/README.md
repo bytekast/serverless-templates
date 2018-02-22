2165c4c34d938e86f2806250a0810d9f214d8113
Rowell Belen <developer@bytekast.com>
2018-02-21 21:58:25 -0700

    Add README, simple test

---
+++ minimal/README.md
@@ -0,0 +1,6 @@
+# Minimal Serverless Groovy Project
+
+To create a project using this template, run
+```
+serverless create --template-url https://github.com/bytekast/serverless-templates/tree/master/minimal --path myservice
+```
---
+++ minimal/src/test/groovy/FunctionsSpec.groovy
@@ -0,0 +1,16 @@
+import spock.lang.Specification
+
+class FunctionsSpec extends Specification {
+
+  def 'test hello function'() {
+
+    given:
+    def functions = new Functions()
+
+    when:
+    def result = functions.hello()
+
+    then:
+    result == 'Hello'
+  }
+}
